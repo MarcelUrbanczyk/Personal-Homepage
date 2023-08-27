c9490fd17f5b65b51fb47333d319f15b9bc347c1
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-27 14:26:03 +0200

    portrait component styled

---
+++ src/features/Profile/Portrait/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+  width: 80%;
+`;
+
+export const Frame = styled.div`
+  align-items: center;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  width: 398px;
+  height: 398px;
+  overflow: hidden;
+  margin-right: 66px;
+`;
