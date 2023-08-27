a212c2025a2becf66a67bfd802abae91dee6a7b2
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-27 19:18:23 +0200

    skillset added

---
+++ src/features/Skills/skillset.js
@@ -0,0 +1,33 @@
+export const currentSkills = [
+  "Semantic & accessible HTML",
+  "Responsive Web Design",
+  "Teamwork",
+  "Markdown",
+  "Immutability",
+  "CSS BEM",
+  "CSS Grid",
+  "CSS Flexbox",
+  "React Router",
+  "Redux-Saga",
+  "Redux Toolkit",
+  "React Hooks",
+  "Error handling",
+  "Working with API (fetch, axios)",
+  "JavaScrip: ES6+",
+  "Promises, Async/Await",
+  "GitHub Pull Requests & Review",
+  "Trello",
+  "Scrum",
+  "NPM",
+  "React",
+  "Git",
+];
+
+export const futureSkills = [
+  "TypeScript",
+  "JS classes",
+  "Cypress",
+  "Node.js",
+  "React Context",
+  "Unit testing",
+];
