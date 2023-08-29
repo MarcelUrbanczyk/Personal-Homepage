2c489cc8211ba315596aa54f008a2e061d0b75b2
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-30 00:38:20 +0200

    projects render fixed

--- src/features/PersonalHomepage/Portfolio/Projects/index.js
+++ src/features/PersonalHomepage/Portfolio/Projects/index.js
@@ -4,13 +4,13 @@ import LinkList from "./LinkList";
 const Projects = ({ projects }) => {
   return (
     <Wrapper>
-      {projects.map(({ title, description, codeUrl, demoUrl }) => {
+      {projects.map(({ title, description, demoUrl, codeUrl }) => (
         <Tile key={title}>
           <Title>{title}</Title>
           <Description>{description}</Description>
           <LinkList codeUrl={codeUrl} demoUrl={demoUrl} />
-        </Tile>;
-      })}
+        </Tile>
+      ))}
     </Wrapper>
   );
 };
