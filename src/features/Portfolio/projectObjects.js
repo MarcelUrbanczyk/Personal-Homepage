e6d54878a465a4b94d9d45f2e2aa1aeba7e4f5ad
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-29 23:36:56 +0200

    created projects as objects

---
+++ src/features/Portfolio/projectObjects.js
@@ -0,0 +1,34 @@
+export const projectObjects = [
+  {
+    title: "Movie Browser",
+    description: `Project description, e.g. website where you can search for favourite
+        movies and people. Project description, e.g. website where you can
+        search.`,
+    demoUrl: "https://link.demo.com",
+    codeUrl: "https://link.code.com",
+  },
+  {
+    title: "Movie Browser",
+    description: `Project description, e.g. website where you can search for favourite
+        movies and people. Project description, e.g. website where you can
+        search.`,
+    demoUrl: "https://link.demo.com",
+    codeUrl: "https://link.code.com",
+  },
+  {
+    title: "Movie Browser",
+    description: `Project description, e.g. website where you can search for favourite
+        movies and people. Project description, e.g. website where you can
+        search.`,
+    demoUrl: "https://link.demo.com",
+    codeUrl: "https://link.code.com",
+  },
+  {
+    title: "Movie Browser",
+    description: `Project description, e.g. website where you can search for favourite
+        movies and people. Project description, e.g. website where you can
+        search.`,
+    demoUrl: "https://link.demo.com",
+    codeUrl: "https://link.code.com",
+  },
+];
