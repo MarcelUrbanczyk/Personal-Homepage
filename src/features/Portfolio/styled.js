28c6cf22535038527b860cdcd526a3864c28eb97
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-29 23:47:28 +0200

    less code

--- src/features/Portfolio/styled.js
+++ src/features/Portfolio/styled.js
@@ -2,14 +2,11 @@ import { styled } from "styled-components";
 import { ReactComponent as ghIcon } from "../../assets/social icons/mark-github.svg";
 
 export const GithubIcon = styled(ghIcon)`
-  width: 40px;
-  height: 40px;
   margin: 0 0 12px 0;
 `;
 
 export const Wrapper = styled.section`
   margin: 0 0 120px 0;
-  padding: 0;
   justify-content: center;
   align-items: center;
   display: flex;
@@ -26,7 +23,6 @@ export const Header = styled.h2`
 export const Description = styled.p`
   margin: 0 0 24px 0;
   font-size: 20px;
-  font-weight: 400;
   line-height: 1.4;
   letter-spacing: 1px;
 `;
