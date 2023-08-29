5326183d024ccb62deee3b6f176a61eefc5401b8
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-29 23:36:08 +0200

    added padding to body instead margin in header

--- src/features/Profile/styled.js
+++ src/features/Profile/styled.js
@@ -1,22 +1,14 @@
 import styled from "styled-components";
-import { ReactComponent as Message } from "../../assets/Message.svg";
-
-export const MailIcon = styled(Message)`
-  width: 24px;
-  height: 24px;
-`;
 
 export const Container = styled.div`
   display: grid;
   grid-template-columns: auto 1fr;
-  margin: 115px 0 63px 0;
+  margin: 0 0 63px 0;
 `;
 
 export const ThisIs = styled.h2`
   color: ${({ theme }) => theme.colors.gray};
-  font-family: Inter;
   font-size: 12px;
-  font-weight: 700;
   line-height: 130%;
   margin-top: 68px;
   text-transform: uppercase;
@@ -24,10 +16,8 @@ export const ThisIs = styled.h2`
 
 export const Name = styled.h1`
   color: ${({ theme }) => theme.colors.black};
-  font-family: Inter;
   font-size: 38px;
   font-weight: 900;
-  line-height: normal;
   letter-spacing: 1.9px;
   margin: 0;
 `;
@@ -38,26 +28,8 @@ export const Wrapper = styled.div`
 
 export const Description = styled.p`
   color: ${({ theme }) => theme.colors.gray};
-  font-family: Inter;
   font-size: 20px;
-  font-style: normal;
-  font-weight: 400;
   line-height: 1.4;
   letter-spacing: 1px;
   margin: 35px 0 0;
-`;
-
-export const HireButton = styled.button`
-  background-color: ${({ theme }) => theme.colors.blue};
-  color: ${({ theme }) => theme.colors.white};
-  border: none;
-  border-radius: 4px;
-  display: inline-flex;
-  padding: 12px 16px;
-  justify-content: center;
-  align-items: center;
-  gap: 16px;
-  font-size: 20.058px;
-  font-family: "Inter";
-  margin-top: 32px;
 `;
