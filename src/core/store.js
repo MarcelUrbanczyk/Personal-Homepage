ee9b55f4c2e3e11f676bf256a07f31909a4f4300
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-30 23:32:58 +0200

    added portfolio reducer to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import portfolioSliceReducer from "../features/PersonalHomepage/Portfolio/portfolioSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducers: {},
+  reducer: {
+    portfolio: portfolioSliceReducer,
+  },
   sagaMiddleware: [sagaMiddleware],
 });
 
