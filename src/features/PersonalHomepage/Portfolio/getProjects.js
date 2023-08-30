23843614d876d441e159005aac6c751f979941b1
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-31 00:38:21 +0200

    created getProjects function

---
+++ src/features/PersonalHomepage/Portfolio/getProjects.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const url = "https://api.github.com/users/MarcelUrbanczyk/repos";
+
+export const getProjects = async () => {
+  const response = await axios.get(url);
+  if (!response.ok) new Error(response.statusText);
+
+  return await response.json();
+};
