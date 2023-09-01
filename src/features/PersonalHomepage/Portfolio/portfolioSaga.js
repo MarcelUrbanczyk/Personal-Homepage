fa8af9fba0dc19435a443313064080d6f4606abc
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-01 18:27:05 +0200

    added longer delay and removed console logs

--- src/features/PersonalHomepage/Portfolio/portfolioSaga.js
+++ src/features/PersonalHomepage/Portfolio/portfolioSaga.js
@@ -9,14 +9,10 @@ import {
 
 function* fetchProjectsHandler() {
   try {
-    console.log("1");
     yield put(fetchProjectsLoading());
-    yield delay(1000);
-    console.log("2");
+    yield delay(2000); //to show loading screen
     const projects = yield call(getProjects);
-    console.log("3");
     yield put(fetchProjectsSuccess(projects));
-    console.log("4");
   } catch (error) {
     yield put(fetchProjectsError());
   }
