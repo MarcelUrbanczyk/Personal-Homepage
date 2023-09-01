c080df4940d66a28adbd735753f20e361ced059c
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-01 18:13:10 +0200

    created global style

--- src/core/App.js
+++ src/core/App.js
@@ -1,13 +1,15 @@
 import { ThemeProvider } from "styled-components";
-import { theme } from "../common/theme/theme";
+import { lightTheme } from "../common/theme/theme";
 import PersonalHomepage from "../features/PersonalHomepage";
 import { Provider } from "react-redux";
 import store from "./store";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
         <PersonalHomepage />
       </ThemeProvider>
     </Provider>
---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  width: 1216px;
+  margin: auto;
+  background-color: ${({ theme }) => theme.colors.background.siteBackground};
+  box-sizing: border-box;
+  font-family: "Inter";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 115px 0 109px 0;
+}
+`;
+
+export default GlobalStyle;
