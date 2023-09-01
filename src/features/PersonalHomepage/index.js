d0d3a6e5d63d890d6f45e487857e34abedea8b6a
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-01 18:14:09 +0200

    created theme switch component

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -2,9 +2,11 @@ import Header from "./Header";
 import Skills from "./Skills";
 import Portfolio from "./Portfolio";
 import Footer from "./Footer";
+import ThemeSwitch from "./ThemeSwitch";
 
 const PersonalHomepage = () => (
   <>
+    <ThemeSwitch />
     <Header />
     <Skills />
     <Portfolio />
