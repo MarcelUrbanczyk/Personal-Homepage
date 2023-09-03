6c0d00a67b9fefef2932a8a5cf36c2494a24e5c9
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-03 19:15:50 +0200

    added theme reducer to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import portfolioReducer from "../features/PersonalHomepage/Portfolio/portfolioSlice";
+import themeReducer from "../common/theme/themeSlice";
 import { rootSaga } from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     portfolio: portfolioReducer,
+    theme: themeReducer,
   },
   middleware: [sagaMiddleware],
 });
