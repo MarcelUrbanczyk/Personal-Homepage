95624edf00ccd0789bd194d55688ce3254770bf4
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-13 20:41:29 +0200

    removed redux saga from app

--- src/core/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import { portfolioSaga } from "../features/PersonalHomepage/Portfolio/portfolioSaga";
-
-export function* rootSaga() {
-  yield all([portfolioSaga()]);
-}
