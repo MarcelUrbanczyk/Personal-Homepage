80419fa0fbb5ac01ef0b70d86e0dbb5347df55e7
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-13 20:41:49 +0200

    removed redux saga from app

--- src/core/store.js
+++ src/core/store.js
@@ -1,19 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import createSagaMiddleware from "redux-saga";
-import portfolioReducer from "../features/PersonalHomepage/Portfolio/portfolioSlice";
 import themeReducer from "../common/theme/themeSlice";
-import { rootSaga } from "./rootSaga";
-
-const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
-    portfolio: portfolioReducer,
     theme: themeReducer,
   },
-  middleware: [sagaMiddleware],
 });
-
-sagaMiddleware.run(rootSaga);
 
 export default store;
