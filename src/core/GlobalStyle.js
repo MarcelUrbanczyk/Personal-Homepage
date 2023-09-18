937e9e5792eae40099fcb114707c3ac6afec467f
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-18 21:17:36 +0200

    deleted witdh for responsivity

--- src/core/GlobalStyle.js
+++ src/core/GlobalStyle.js
@@ -2,17 +2,18 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 body {
-  width: 1216px;
+  max-width: 1216px;
   margin: auto;
   background-color: ${({ theme }) => theme.colors.background.siteBackground};
   box-sizing: border-box;
   font-family: "Inter";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  padding: 115px 0 109px 0;
+  padding: 115px 20px 109px 20px;
   transition: background-color 0.3s;
   word-break: break-word;
 }
+
 `;
 
 export default GlobalStyle;
