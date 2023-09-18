40d2a97ad883eb559eb49d6a3964e8f752d735a7
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-18 22:50:01 +0200

    less code

--- src/core/GlobalStyle.js
+++ src/core/GlobalStyle.js
@@ -9,7 +9,7 @@ body {
   font-family: "Inter";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  padding: 115px 20px 109px 20px;
+  padding: 110px 20px;
   transition: background-color 0.3s;
   word-break: break-word;
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
